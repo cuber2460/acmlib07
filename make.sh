58e1b4dd9bfed52ccb238637c62a36f309ad3ed4
Marek Sommer <mareksommer7@gmail.com>
2016-10-23 17:07:43 +0200

    Poprawiony format Ukkonena, Lyndona.  Poprawa skryptu make.sh

--- make.sh
+++ make.sh
@@ -6,35 +6,39 @@ LIBFILE=lib.pdf
 echo -n "" > $ALLFILE
 
 function make_name {
-	echo ${1%.*} | sed 's/_/ /g'
+  echo ${1%.*} | sed 's/_/ /g'
 }
 
 function print_header {
-	NAME=`make_name $1`
-	WIDTH=74
-	printf "/*****************************************************************************/\n"
-	printf "/*                                                                           */\n"
-	printf "/* %*s%*s*/\n" $(((${#NAME}+$WIDTH)/2)) "$NAME" $(($WIDTH-((${#NAME}+$WIDTH)/2))) ""
-	printf "/*                                                                           */\n"
-	printf "/*****************************************************************************/\n"
+  NAME=`make_name $1`
+  WIDTH=$((80 - 8))
+  LEFT=$((($WIDTH - ${#NAME}) / 2))
+  RIGHT=$(($WIDTH - ${#NAME} - $LEFT))
+  printf "// -------------------------------------------------------------------------- //\n"
+  printf "// "
+  printf '%*.*s' 0 $LEFT $(printf '%0.1s' "-"{1..80})
+  printf " $NAME "
+  printf '%*.*s' 0 $RIGHT $(printf '%0.1s' "-"{1..80})
+  printf " //\n"
+  printf "// -------------------------------------------------------------------------- //\n"
 }
 
 for j in *; do
-	if [ -d "$j" ]; then
-		cd "$j"
-		for i in *.cpp; do
-			print_header $i >> ../$ALLFILE
-			cat $i >> ../$ALLFILE
-		done
-		cd ..
-	fi
+  if [ -d "$j" ]; then
+    cd "$j"
+    for i in *.cpp; do
+      print_header $i >> ../$ALLFILE
+      cat $i >> ../$ALLFILE
+    done
+    cd ..
+  fi
 done
 
 TEMP=.tmp.cpp
 iconv -f utf-8 -t iso-8859-2 $ALLFILE -o $TEMP
 mv $TEMP $ALLFILE
 
-a2ps -P pdf $ALLFILE -L 65 --center-title="University of Warsaw" --footer="" --left-footer="" --right-footer="" --header="" --encoding=latin2
+a2ps -P pdf $ALLFILE -L 65 -l 80 --center-title="University of Warsaw" --footer="" --left-footer="" --right-footer="" --header="" --encoding=latin2
 
 #a2ps $ALLFILE --file-align=fill -L 65  --margin=0 -o $LIBFILE --left-title="University of Warsaw" --center-title="" --right-title="" --header="Page %s./%s#"
 
--- marek/Ukkonen.cpp
+++ marek/Ukkonen.cpp
@@ -1,159 +1,133 @@
-struct Ukkonen
-{
-	typedef char znak;
-	static const int max_len = 1000000;
+template <typename Char>
+struct Ukkonen {
+  // Musi być ściśle większe niż jakakolwiek długość słowa.
+  static constexpr int kInfinity = numeric_limits<int>::max();
 
-	struct node
-	{
-		map<znak, pair<node*, pair<int, int> > > trans;
-		node * suflink;
+  struct Node {
+    map<Char, pair<Node*, pair<int, int>>> transition;
+    Node* suflink;
+  };
 
-		// wypisuje drzewo - nie trzeba przepisywac!
-		string getstr(int a, int b, znak * nap)
-		{
-			string s;
-			for(int i = a; i <= b; i++)
-				s += nap[i];
-			return s;
-		}
+  // Ta metoda jest wywoływana zawsze gdy tworzona jest krawędź {node}[a, +oo).
+  void CreateLeafCallback(Node* node, int a) {
+    // printf("CreateLeafCallback({%p}[%d, +oo))\n", node, a);
+  }
 
-		// wypisuje drzewo - nie trzeba przepisywac!
-		void wypisz(znak * nap, string s = "")
-		{
-			printf("%s: %p, suflink -> %p\n", s.c_str(), this, suflink);
-			for(map<znak, pair<node*, pair<int, int> > >::iterator i = trans.begin(); i != trans.end(); i++)
-			{
-				for(int j = 0; j < (int) s.length(); j++)
-					printf(" ");
-				printf("(%c, %p, %d, %d [%s])\n", (*i).first, (*i).second.first, (*i).second.second.first, (*i).second.second.second, getstr((*i).second.second.first, (*i).second.second.second, nap).c_str());
-				if((*i).second.first != nullptr)
-					(*i).second.first->wypisz(nap, s + getstr((*i).second.second.first, (*i).second.second.second, nap));
-			}
-		}
-	};
+  // Ta metoda jest wywoływana zawsze gdy krawędź {node}[a, b] zamienia się
+  // w dwie krawędzie: {node}[a, c-1], {middle}[c, b].
+  void SplitEdgeCallback(Node* node, int a, int b, Node* middle, int c) {
+    // printf("SplitEdgeCallback({%p}[%d, %d] -> {%p}[%d, %d] + {%p}[%d, %d])\n",
+    //       node, a, b, node, a, c - 1, middle, c, b);
+  }
 
-	int n;
-	znak nap[max_len + 5]; // słowo powinno zajmować indeksy [1..n]
-	node * root;
-	node * pin;
+  // vector<unique_ptr<Node>> nodes_to_delete;  // Odkomentować w celu usuwania.
+  Node* NewNode() {
+    Node* node = new Node();
+    // nodes_to_delete.insert(node);            // Odkomentować w celu usuwania.
+    return node;
+  }
 
-	Ukkonen() : n(0), root(nullptr), pin(nullptr) {}
+  vector<Char> text;  // Słowo powinno zajmować indeksy [0..n-1].
+  Node* root;
+  Node* pin;
+  Node* last_explicit_node;  // Ostatni wierzchołek ,,explicit''.
+  int last_length;    // Liczba literek do ostatniego wierzchołka ,,implicit''.
 
-	void kanonizuj(node * & s, int & a, int b)
-	{
-		if(b < a)
-			return;
-		pair<node*, pair<int, int> > t = s->trans[nap[a]];
-		node * sp = t.first;
-		int ap = t.second.first;
-		int bp = t.second.second;
-		while(bp - ap <= b - a)
-		{
-			a = a + bp - ap + 1;
-			s = sp;
-			if(a <= b)
-			{
-				t = s->trans[nap[a]];
-				sp = t.first;
-				ap = t.second.first;
-				bp = t.second.second;
-			}
-		}
-	}
+  // "reserve" warto ustawić na maksymalną dlugość słowa, ale wcale nie trzeba.
+  Ukkonen(const int reserve = 0) : root(nullptr), pin(nullptr) {
+    text.reserve(reserve);
+    root = NewNode();
+    pin = NewNode();
+    root->suflink = pin;
+    last_explicit_node = root;
+    last_length = 0;
+  }
 
-	bool splituj(node * s, int a, int b, znak c, node * & ret)
-	{
-		if(a <= b)
-		{
-			pair<node*, pair<int, int> > & t = s->trans[nap[a]];
-			node * sp = t.first;
-			int ap = t.second.first;
-			int bp = t.second.second;
-			if(c == nap[ap + b - a + 1])
-			{
-				ret = s;
-				return true;
-			}
-			ret = new node();
-			t.second.second = ap + b - a;
-			t.first = ret;
-			ret->trans[nap[ap + b - a + 1]] = make_pair(sp, make_pair(ap + b - a + 1, bp));
-			return false;
-		}
-		ret = s;
-		return s->trans.find(c) != s->trans.end();
-	}
+  void Canonize(Node** s, int* a, int b) {
+    if (b < *a) return;
+    pair<Node*, pair<int, int>> t = (*s)->transition[text[*a]];
+    Node* sp = t.first;
+    int ap = t.second.first;
+    int bp = t.second.second;
+    while (bp - ap <= b - *a) {
+      *a = *a + bp - ap + 1;
+      *s = sp;
+      if (*a <= b) {
+        t = (*s)->transition[text[*a]];
+        sp = t.first;
+        ap = t.second.first;
+        bp = t.second.second;
+      }
+    }
+  }
 
-	void update(node * & s, int & a, int & i)
-	{
-		node * oldr = root;
-		node * r;
-		bool end = splituj(s, a, i - 1, nap[i], r);
-		while(!end)
-		{
-			r->trans[nap[i]] = make_pair(nullptr, make_pair(i, n));
-			if(oldr != root)
-				oldr->suflink = r;
-			oldr = r;
-			s = s->suflink;
-			kanonizuj(s, a, i - 1);
-			end = splituj(s, a, i - 1, nap[i], r);
-		}
-		if(oldr != root)
-			oldr->suflink = s;
-	}
+  bool TestAndSplit(Node* s, int a, int b, Char c, Node** ret) {
+    if (a <= b) {
+      pair<Node*, pair<int, int>>& t = s->transition[text[a]];
+      Node* sp = t.first;
+      int ap = t.second.first;
+      int bp = t.second.second;
+      if (c == text[ap + b - a + 1]) {
+        *ret = s;
+        return true;
+      }
+      *ret = NewNode();
+      t.second.second = ap + b - a;
+      t.first = *ret;
+      (*ret)->transition[text[ap + b - a + 1]] =
+          make_pair(sp, make_pair(ap + b - a + 1, bp));
+      SplitEdgeCallback(s, ap, bp, *ret, ap + b - a + 1);
+      return false;
+    }
+    *ret = s;
+    return s->transition.find(c) != s->transition.end();
+  }
 
-	void konstruuj()
-	{
-		root = new node();
-		pin = new node();
-		for(int i = 1; i <= n; i++)
-			pin->trans[nap[i]] = make_pair(root, make_pair(i, i));
-		root->suflink = pin;
-		node * s = root;
-		int a = 1;
-		for(int i = 1; i <= n; i++)
-		{
-			update(s, a, i);
-			kanonizuj(s, a, i);
-		}
-	}
+  void Update(Node** s, int* a, int i) {
+    Node* oldr = root;
+    Node* r;
+    bool end = TestAndSplit(*s, *a, i - 1, text[i], &r);
+    while (!end) {
+      CreateLeafCallback(r, i);
+      r->transition[text[i]] = make_pair(nullptr, make_pair(i, kInfinity));
+      if (oldr != root) oldr->suflink = r;
+      oldr = r;
+      *s = (*s)->suflink;
+      Canonize(s, a, i - 1);
+      end = TestAndSplit(*s, *a, i - 1, text[i], &r);
+    }
+    if (oldr != root) oldr->suflink = *s;
+  }
 
-	// wypisuje slowa w kolejnosci alfabetycznej, nie trzeba przepisywac!
-	void podslowa(node * v = nullptr, string s = "")
-	{
-		if(v == nullptr)
-			v = root;
-		for(auto i : v->trans)
-		{
-			string f = s;
-			for(int j = i.second.second.first; j <= i.second.second.second; j++)
-			{
-				f += nap[j];
-				printf("%s\n", f.c_str());
-			}
-			if(i.second.first != nullptr)
-				podslowa(i.second.first, f);
-		}
-	}
+  // Dodaje kolejną literę do drzewa.
+  void AddLetter(Char z) {
+    const int i = static_cast<int>(text.size());
+    text.push_back(z);
+    auto it = pin->transition.find(z);
+    if (it == pin->transition.end())
+      pin->transition[z] = make_pair(root, make_pair(i, i));
+    Update(&last_explicit_node, &last_length, i);
+    Canonize(&last_explicit_node, &last_length, i);
+  }
 
-	// nie trzeba przepisywac!
-	void wypisz()
-	{
-		printf("pin = %p, root = %p\n", pin, root);
-		if(pin != nullptr)
-			pin->wypisz(nap);
-	}
+  // Zamienia wszystkie krawędzie: [x, +oo) -> [x, text.size()-1].
+  void ClearInfinities(Node* node = nullptr) {
+    if (node == nullptr) node = root;
+    for (auto& it : node->transition) {
+      if (it.second.second.second == kInfinity)
+        it.second.second.second = static_cast<int>(text.size()) - 1;
+      else ClearInfinities(it.second.first);
+    }
+  }
 };
 
-/* Przykład użycia */
-int main()
-{
-	Ukkonen u;
-	scanf("%s", u.nap + 1);
-	u.n = strlen(u.nap + 1);
-	u.konstruuj();
-	u.podslowa();
-	//u.wypisz();
-	return 0;
+template <typename Char>
+constexpr int Ukkonen<Char>::kInfinity;
+
+int main() {  // Przykład użycia.
+  string s = "abcdefgh#";
+  Ukkonen<char> u(s.size() /* reserve */);
+  for (char c : s) u.AddLetter(c);
+  u.ClearInfinities();
+  return 0;
 }
