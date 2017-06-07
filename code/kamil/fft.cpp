480c1fb8882317e4829ccc3acd52886b55799841
Errichto <kamil486@gmail.com>
2017-06-07 15:52:15 +0200

    FFT bez baby-giant, ale z 2 wywolaniami

--- code/kamil/fft.cpp
+++ code/kamil/fft.cpp
@@ -6,7 +6,7 @@ So integer rounding works for doubles with answers 0.5*1e15,
 e.g. for sizes 2^20 and RANDOM positive integers up to 45k.
 Those values assume DBL_MANT_DIG=53 and LDBL_MANT_DIG=64.
 For input in [0, M], you can decrease everything by M/2.
-For many small vectors, use brute-force and step = 4.*/
+If there are many small vectors, uncomment "BRUTE FORCE".*/
 typedef double ld; // 'long double' is 2.2 times slower
 struct C {
 	ld real, imag;
@@ -14,79 +14,95 @@ struct C {
 		return C{real * he.real - imag * he.imag,
 				real * he.imag + imag * he.real};
 	}
+	void operator += (const C & he) {
+		real += he.real; imag += he.imag;
+	}
 };
 void dft(vector<C> & a, bool rev) {
 	const int n = a.size();
 	for(int i = 1, k = 0; i < n; ++i) {
-		for(int bit = n / 2; !((k ^= bit) & bit); bit /= 2);
+		for(int bit = n / 2; (k ^= bit) < bit; bit /= 2);;;
 		if(i < k) swap(a[i], a[k]);
 	}
-	for(int len = 2; len <= n; len *= 2) {
-		const ld ang = 4 * acosl(0) / len * (rev ? -1 : 1);
-		const int step = 64; // step = 4 if many small vectors
-		vector<C> baby(step); // step = 1 to check samples
-		REP(i, step) baby[i] = C{cos(ang*i), sin(ang*i)};
-		vector<C> giant;
-		for(int i = 0; i < len / 2; i += step)
-			giant.push_back(C{cos(ang*i), sin(ang*i)});
-		for(int i = 0; i < n; i += len) {
-			REP(k, len / 2) {
-				const C x = a[i+k], y = a[i+k+len/2] * (k >= step ?
-					giant[k/step] * baby[k%step] : baby[k%step]);
-				a[i+k] = C{x.real + y.real, x.imag + y.imag};
-				a[i+k+len/2] = C{x.real - y.real, x.imag - y.imag};
-	}}}
+	for(int len = 1, who = 0; len < n; len *= 2, ++who) {
+		static vector<C> t[30];
+		vector<C> & om = t[who];
+		if(om.empty()) {
+			om.resize(len);
+			const ld ang = 2 * acosl(0) / len;
+			REP(i, len) om[i] = i%2 || !who ?
+					C{cos(i*ang), sin(i*ang)} : t[who-1][i/2];
+		}
+		for(int i = 0; i < n; i += 2 * len)
+			REP(k, len) {
+				 const C x = a[i+k], y = a[i+k+len]
+						* C{om[k].real, om[k].imag * (rev ? -1 : 1)};
+				a[i+k] += y;
+				a[i+k+len] = C{x.real - y.real, x.imag - y.imag};
+			}
+	}
 	if(rev) REP(i, n) a[i].real /= n;
 }
-template<typename T>vector<T> multiply(const vector<T> & a, const vector<T> & b) {
+template<typename T>vector<T> multiply(const vector<T> & a, const vector<T> & b, bool split = false) {
 	if(a.empty() || b.empty()) return {};
 	int n = a.size() + b.size();
 	vector<T> ans(n - 1);
-/*if(min(a.size(),b.size())<200){//use if many small vectors
+	/* if(min(a.size(),b.size()) < 190) { // BRUTE FORCE
 		REP(i, a.size()) REP(j, b.size()) ans[i+j] += a[i]*b[j];
-		return ans;} */
-	while(n&(n-1)) ++n;	
-	vector<C> one(n), two(n); // standard version starts here
-	REP(i, a.size()) one[i].real = a[i];
-	REP(i, b.size()) two[i].real = b[i];
-	dft(one, false);
-	dft(two, false);
-	REP(i, n) one[i] = one[i] * two[i];
-	dft(one, true);
-	REP(i, ans.size()) ans[i] = is_integral<T>::value ?
-			llround(one[i].real) : one[i].real;
-	// You can split big INTEGERS into pairs a1*M+a2,
-/*const T M = 150; // where M = sqrt(max_absvalue).
-	vector<C> t[2][2]; // This version is 7/3 times slower.
-	REP(x,2)REP(y,2) { // Watch out for MLE (big constant).
-		t[x][y].resize(n); auto & in = x ? b : a;
-		REP(i,in.size()) t[x][y][i].real= y ? in[i]/M : in[i]%M;
-		dft(t[x][y], false);
+		return ans; } */
+	while(n&(n-1)) ++n;
+	// http://codeforces.com/blog/entry/48417
+	auto speed = [&](const vector<C> & w, int i, int k) {
+		int j = i ? n - i : 0, r = k ? -1 : 1;
+		return C{w[i].real + w[j].real * r, w[i].imag
+				- w[j].imag * r} * (k ? C{0, -0.5} : C{0.5, 0});
+	};
+	if(!split) { // standard fast version
+		vector<C> in(n), done(n);
+		REP(i, a.size()) in[i].real = a[i];
+		REP(i, b.size()) in[i].imag = b[i];
+		dft(in, false);
+		REP(i, n) done[i] = speed(in, i, 0) * speed(in, i, 1);
+		dft(done, true);
+		REP(i, ans.size()) ans[i] = is_integral<T>::value ?
+				llround(done[i].real) : done[i].real;
+	//REP(i,ans.size())err=max(err,abs(done[i].real-ans[i]));
 	}
-	REP(s, 3) {
-		vector<C> prod(n);
-		REP(x, 2) REP(y, 2) if(x + y == s) REP(i, n) {
-			C he = t[0][x][i] * t[1][y][i];
-			prod[i].real += he.real; prod[i].imag += he.imag;
+	else { // Split big INTEGERS into pairs a1*M+a2,
+		const T M = 150; // where M = sqrt(max_absvalue).
+		vector<C> t[2]; // This version is 2.2-2.5 times slower.
+		REP(x, 2) {
+			t[x].resize(n);
+			auto & in = x ? b : a;
+			REP(i, in.size()) t[x][i]=C{ld(in[i]%M), ld(in[i]/M)};
+			dft(t[x], false);
 		}
-		dft(prod, true);
-		REP(i, ans.size()) ans[i] +=
-				llround(prod[i].real) * my_integer_power(M, s);
-	}*/
+		T mul = 1;
+		for(int s = 0; s < 3; ++s, mul *= M) {
+			vector<C> prod(n);
+			REP(x, 2) REP(y, 2) if(x + y == s) REP(i, n)
+				prod[i] += speed(t[0], i, x) * speed(t[1], i, y);
+			dft(prod, true);
+			REP(i, ans.size()) ans[i]+= llround(prod[i].real)*mul;
+		}
+	}
 	return ans;
 }
 
 typedef long long ll;
 int main() {
-	const int n = 500 * 1000;
-	const int M = 25 * 1000;
-	vector<ll> a, b;
-	for(int i = 0; i < n; ++i) {
-		a.push_back(rand() % M);
-		b.push_back(rand() % M);
+	REP(_, 1) {
+		const int n = 500 * 1000;
+		const int M = 30123;
+		vector<ll> a, b;
+		for(int i = 0; i < n; ++i) {
+			a.push_back(rand() % M);
+			b.push_back(rand() % M);
+		}
+		auto ans = multiply(a, b, 0);
+		for(int i = 0; i < min(10, (int) ans.size()); ++i)
+			cout << ans[i] << " ";
+		cout << "\n";
 	}
-	auto ans = multiply(a, b);
-	for(int i = 0; i < min(10, (int) ans.size()); ++i)
-		cout << ans[i] << " ";
-	cout << "\n";
+	//~ printf("%.10Lf\n", (long double) err);
 }
