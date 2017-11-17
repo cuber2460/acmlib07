a8aec66a36db030c1f0e64dc6397ee3ab887fd34
Marek Sommer <mareksommer7@gmail.com>
2017-11-17 22:03:21 +0100

    Skróciłem Manachera o prawie 3.5%.

--- code/marek/Manacher.cpp
+++ code/marek/Manacher.cpp
@@ -1,48 +1,13 @@
-// Wypełnia tablicę r1 promieniami nieparzystych palindromów. Przykład:
-// nap: a b a c a b a
-//  r1: 0 1 0 3 0 1 0
-void Manacher1(char* nap, int* r1, int n) {
-  if (n == 0) return;
-  int i = 0, k = 1;
-  r1[0] = 0;
-  while (i < n) {
-    while (k <= r1[i] and i + k + 1 < n and r1[i - k] != r1[i] - k) {
-      r1[i + k] = min(r1[i - k], r1[i] - k);
-      k++;
-    }
-    if (i + k >= n) break;
-    r1[i + k] = r1[i] - k;
-    i += k;
-    k = 1;
-    while ((i + r1[i] + 1 < n) and (i - r1[i] - 1 >= 0) and
-           nap[i + r1[i] + 1] == nap[i - r1[i] - 1]) {
-      r1[i]++;
-    }
-  }
-}
-
-// Wypełnia tablicę r2 promieniami parzystych palindromów. Na pozycji i-tej
-// znajduje się promień palindromów, które mają środek pomiędzy literkami (i-1)
-// oraz i. To na przyklad oznacza, ze zawsze r2[0] = 0 (o ile slowo nie jest
-// puste). Przyklad:
-// nap: a b b a a b b
-//  r2: 0 0 2 0 3 0 1
-void Manacher2(char* nap, int* r2, int n) {
-  if (n == 0) return;
-  int i = 0, k = 1;
-  r2[0] = 0;
-  while (i < n) {
-    while (k < r2[i] and i + k < n and r2[i - k] != r2[i] - k) {
-      r2[i + k] = min(r2[i - k], r2[i] - k);
-      k++;
-    }
-    if (i + k >= n) break;
-    r2[i + k] = max(0, r2[i] - k);
-    i += k;
-    k = 1;
-    while ((i + r2[i] < n) and (i - r2[i] - 1 >= 0) and
-           nap[i + r2[i]] == nap[i - r2[i] - 1]) {
-      r2[i]++;
-    }
+// @s[0..n-1]  - napis długości @n.
+// @r[0..2n-2] - tablica promieni palindromów.
+// s: a   b   a   a   b   a   a   c   a   a   b   b   b   b   a   a   c   a   c
+// r: 0 0 1 0 0 3 0 0 2 0 0 1 0 0 3 0 0 1 0 0 0 1 1 6 1 1 0 0 0 1 0 0 1 0 1 0 0
+void Manacher(const char* s, int n, int* r) {
+  for (int i = 0, m = 0, k = 0, p = 0; i < 2 * n - 1; m = i++ - 1) {
+    while (p < k and i / 2 + r[m] != k)
+      r[i++] = min(r[m--], (k + 1 - p++) / 2);
+    while (k + 1 < n and p > 0 and s[k + 1] == s[p - 1])
+      k++, p--;
+    r[i] = (k + 1 - p++) / 2;
   }
 }
