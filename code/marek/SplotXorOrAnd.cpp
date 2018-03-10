e2fbb4fd5e7905b3ca81c3bbdc2fb3d342bd578e
Marek Sommer <mareksommer7@gmail.com>
2018-03-10 02:11:23 +0100

    Dodałem splot XOR OR AND.

---
+++ code/marek/SplotXorOrAnd.cpp
@@ -0,0 +1,31 @@
+// TODO: Porządnie przetestować.
+
+constexpr int pot = mid;
+// Xor(a, b, c):
+//   Trans(a) XOR; Trans(b) XOR; Trans(c) XOR
+//   d[i] = a[i] * b[i] * c[i]
+//   Trans(d) XOR ODW
+//   return d
+void Trans(ll a[1 << pot]) {
+  const int n = (1 << pot);
+  for (int len = 1; len < n; len *= 2) {
+    for (int i = 0; i < n; i += 2 * len) {
+      for (int j = 0; j < len; j++) {
+        const ll u = a[i + j];
+        const ll v = a[i + j + len];
+        a[i + j      ] = DodajLiczby(u, v);              // XOR
+        a[i + j + len] = OdejmijLiczby(u, v);            // XOR
+        a[i + j      ] = Podziel2(DodajLiczby(u, v));    // XOR ODW
+        a[i + j + len] = Podziel2(OdejmijLiczby(u, v));  // XOR ODW
+        a[i + j      ] = v;                              // AND
+        a[i + j + len] = DodajLiczby(u, v);              // AND
+        a[i + j      ] = OdejmijLiczby(v, u);            // AND ODW
+        a[i + j + len] = u;                              // AND ODW
+        a[i + j      ] = DodajLiczby(u, v);              // OR
+        a[i + j + len] = u;                              // OR
+        a[i + j      ] = v;                              // OR ODW
+        a[i + j + len] = OdejmijLiczby(u, v);            // OR ODW
+      }
+    }
+  }
+}
