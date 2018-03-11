06c2a334bcd6d8fa37fa4647a14d82ec784a35e6
Marek Sommer <mareksommer7@gmail.com>
2018-03-12 00:04:15 +0100

    Skróciłem pref o 76.5%.

--- code/marek/Pref.cpp
+++ code/marek/Pref.cpp
@@ -1,30 +1,10 @@
-template <typename T>
-int PrefMatching(const T* s, int n, int j, int k) {
-  k = max(k, j);
-  int i = k - j;
-  while (k < n and s[i] == s[k]) {
-    i++;
-    k++;
-  }
-  return k - j;
-}
-
-// Liczy funkcję pref.
-// s - słowo na pozycjach [0, n-1].
-// n - długość słowa.
-// p - tablica pref, która zostanie wypełniona na pozycjach [0, n - 1].
-template <typename T>
-void Pref(const T* s, int n, int* p) {
-  int i = 0;
-  p[0] = -1;
-  for (int j = 1; j < n; j++) {
-    const int l = j - i;
-    if (l < p[i] and j + p[l] < i + p[i]) {
-      p[j] = p[l];
-    } else {
-      p[j] = PrefMatching(s, n, j, i + p[i]);
-      i = j;
-    }
-  }
+void Pref(const char* s, int n, int* p) {
   p[0] = n;
+  int i = 1, m = 0;
+  while (i < n) {
+    while (m + i < n and s[m + i] == s[m]) m++;
+    p[i++] = m;
+    m = max(m - 1, 0);
+    for (int k = 1; p[k] < m; m--) p[i++] = p[k++];
+  }
 }
