d6d864af20d7875ed28364143adf164bd297e2d4
Marek Sommer <mareksommer7@gmail.com>
2018-03-11 22:44:38 +0100

    Skrócone modulo o 13%.

--- code/marek/Modulo_2_to_61_minus_1.cpp
+++ code/marek/Modulo_2_to_61_minus_1.cpp
@@ -1,11 +1,9 @@
 // Zwraca (a * b) % (2**p - 1).
-// Int - typ, który jest w stanie pomieścić a * b.
-template <typename T = unsigned long long, typename Int = __uint128_t>
-T Mnoz(T a, T b) {
+ull Mnoz(ull a, ull b) {
   constexpr int p = 61;
-  constexpr T mod = (T(1) << p) - 1;
-  const auto A = (Int) a * b;
-  T result = (T) (A & mod) + (T) (A >> p);
+  constexpr ull mod = (1llu << p) - 1;
+  const auto A = (__uint128_t) a * b;
+  ull result = (ull) (A & mod) + (ull) (A >> p);
   if (mod <= result) {
     result -= mod;
   }
