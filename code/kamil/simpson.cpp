6cc13a9d1197eb7808af1f93674d4319f3036b87
Errichto <kamil486@gmail.com>
2018-03-31 00:20:00 +0200

    szybszy simpson (całkowanie)

--- code/kamil/simpson.cpp
+++ code/kamil/simpson.cpp
@@ -1,20 +1,26 @@
-// Either run integral(A, B) once or split the interval [A, B] into 100-1000
+// Either run integral(A, B) once or split the interval [A, B] into up to ~1000
 // smaller intervals -- if the function behaves oddly or the interval is long.
-ld _simp(ld low, ld high) {
-	const int n = 500; // decrease n for a speed-up
+ld simp(ld low, ld high, const ld * old, vector<ld> & nowe) {
+	const int n = 500; // n must be even!!! Try n = 2 and n = 10.
+	nowe.resize(n + 1);
 	ld total = 0, jump = (high - low) / n;
 	for(int i = 0; i <= n; ++i) {
-		int mno = 2;
-		if(i & 1) mno = 4;
-		else if(i == 0 || i == n) mno = 1;
-		total += f(low + i * jump) * mno; // uses a global function ld f(ld x)
+		int mul = i == 0 || i == n ? 1 : 2 + i % 2 * 2; // 1 2 4 2 4 ...
+		nowe[i] = !old || i % 2 ? f(low + i * jump) : old[i/2];
+		total += nowe[i] * mul;
 	}
 	return total * (high - low) / n / 3;
 }
-ld integral(ld low, ld high, ld prv = 0, bool known = false) {
-	if(!known) prv = _simp(low, high);
+ld rec(ld low, ld high, ld prv, const vector<ld> & old) {
 	ld mid = (low + high) / 2;
-	ld L = _simp(low, mid), R = _simp(mid, high);
-	if(abs(L + R - prv) < 1e-12L) return L + R; // eps ~ required precision
-	return integral(low, mid, L, true) + integral(mid, high, R, true);
+	vector<ld> left, right;
+	ld L = simp(low, mid, old.data(), left);
+	ld R = simp(mid, high, old.data() + old.size() / 2, right);
+	if(abs(L + R - prv) < 1e-12L) return L + R; // eps ~ required abs precision
+	return rec(low, mid, L, left) + rec(mid, high, R, right);
+}
+ld integral(ld low, ld high) {
+	vector<ld> old;
+	ld prv = simp(low, high, 0, old);
+	return rec(low, high, prv, old);
 }
