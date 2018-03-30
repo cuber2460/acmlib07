56185edf75840861a7f0c80a9cc935cdac40d696
Errichto <kamil486@gmail.com>
2018-03-31 00:48:20 +0200

    krótsze fast_phi

--- code/kamil/fast_phi.cpp
+++ code/kamil/fast_phi.cpp
@@ -1,6 +1,3 @@
-int phi[nax]; // 0.4s for nax = 1e7+5
-for(int i = 0; i < nax; ++i) phi[i] = i;
-for(int i = 2; i < nax; ++i) if(phi[i] == i)
-	for(int j = i; j < nax; j += i)
-		phi[j] = phi[j] / i * (i - 1);
-// phi = {0 1 1 2 2 4 2 6 4 6 4 10 ...}
+// 0.4s for nax = 1e7+5, phi = {0 1 1 2 2 4 2 6 4 6 4 10 ...}
+int phi[nax]; REP(i, nax) phi[i] = i; for(int i = 2; i < nax; ++i)
+	if(phi[i] == i) for(int j = i; j < nax; j += i) phi[j] -= phi[j] / i;
