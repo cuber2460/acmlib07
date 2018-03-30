8014636942fb94616642c3e175cb915d6bdde451
Marek Sommer <mareksommer7@gmail.com>
2018-03-30 17:20:30 +0200

    Link Cut!!!!!!!

--- code/marek/Link_Cut.cpp
+++ code/marek/Link_Cut.cpp
@@ -1,20 +1,15 @@
-struct Value {
-  void touch(Value* left, Value* right) {}
-  void update(Value* left, Value* right) {}
-};
+struct Splay {
+  Splay *l = nullptr, *r = nullptr, *p = nullptr;
+  bool flip = false;
+  int roz = 1;    // SUBTREE  Rozmiar poddrzewa.
+  int axroz = 1;  // SUBTREE  Pomocniczny rozmiar poddrzewa.
 
-struct Splay : Value {
-  Splay *l, *r, *p;
-  bool flip;
-
-  template<typename ...Args>
-  Splay(const Args& ...args)
-    : Value(args...), l(nullptr), r(nullptr), p(nullptr), flip(false) {}
-
-  // Sprawdza, czy wierzchołek jest rootem splejowym.
-  bool isRoot() { return !p or (p->l != this and p->r != this); }
-
-  void update() { Value::update(l, r); }
+  void update() {
+    assert(!flip and (!l or !l->flip) and (!r or !r->flip));
+    axroz = roz;               // SUBTREE
+    if (l) axroz += l->axroz;  // SUBTREE
+    if (r) axroz += r->axroz;  // SUBTREE
+  }
 
   void touch() {
     if (flip) {
@@ -23,118 +18,87 @@ struct Splay : Value {
       if (r) r->flip = !r->flip;
       flip = false;
     }
-    Value::touch(l, r);
   }
 
-  static void connect(Splay* p, Splay* c, bool left) {
-    (left ? p->l : p->r) = c;
-    if (c) c->p = p;
-  }
+  bool sroot() { return !p or (p->l != this and p->r != this); }
+  void connect(Splay* c, bool left) { (left ? l : r) = c; if (c) c->p = this; }
 
   void rotate() {
     Splay* f = p;
     Splay* t = f->p;
-    f->touch();
-    touch();
-    bool isr = f->isRoot();
-    bool left = (this == f->l);
-    connect(f, left ? r : l, left);
-    connect(this, f, !left);
-    if (isr) this->p = t;
-    else connect(t, this, f == t->l);
-    if (f->r) f->r->touch();
-    if (f->l) f->l->touch();
+    const bool isr = f->sroot();
+    const bool left = (this == f->l);
+    f->connect(left ? r : l, left);
+    connect(f, !left);
+    if (isr) p = t;
+    else t->connect(this, f == t->l);
     f->update();
   }
 
+  void push() {
+    sroot() ? touch() : p->push();
+    if (l) l->touch(); if (r) r->touch();
+  }
+
   void splay() {
-    while (!isRoot()) {
+    push();
+    while (!sroot()) {
       Splay* x = p->p;
-      if (!p->isRoot()) (((p->l == this) == (x->l == p)) ? p : this)->rotate();
+      if (!p->sroot()) (((p->l == this) == (x->l == p)) ? p : this)->rotate();
       rotate();
     }
-    touch();
-    if (l) l->touch();
-    if (r) r->touch();
     update();
   }
 
-  static Splay* lastDisc;
-  void expose() {
-    Splay* x = nullptr;
-    lastDisc = nullptr;
-    for (Splay* q = this; q; q = q->p) {
+  // Przenosi wierzchołek do korzenia. Prawe dziecko v będzie równe nullptr.
+  // Aby zrobić coś na ścieżce od korzenia do v:
+  //   >> v->expose();
+  //   >> /* v reprezentuje ścieżkę, można z nim robić co się chce.
+  //   >>  * Jeśli się odwiedzi wierzchołki poniżej v, to na najniższym
+  //   >>  * trzeba wywołać ->splay(); */
+  // Aby znaleźć LCA u i v (muszą być w tym samym poddrzewie):
+  //   >> u->expose(); lca = v->expose();
+  Splay* expose() {
+    Splay *q = this, *x = nullptr;
+    while (q) {
       q->splay();
+      if (q->r) q->roz += q->r->axroz;  // SUBTREE
+      if (x) q->roz -= x->axroz;        // SUBTREE
       q->r = x;
       q->update();
       x = q;
-      lastDisc = q;
+      q = q->p;
     }
     splay();
-  }
-
-  void link(Splay* to) {
-    expose();
-    assert(l == nullptr);  // to powinien być root
-    p = to;
+    return x;
   }
 
   // Zwraca roota drzewowego (nie splejowego!).
   Splay* root() {
     expose();
     Splay* s = this;
-    while (s->l) {
-      s = s->l;
-      s->touch();
-    }
+    while (s->touch(), s->l) s = s->l;
     s->splay();
     return s;
-  }
-
-  // Wywołaj funkcję f na poddrzwie reprezentującym ścieżkę od korzenia
-  // do wierzchołka. Aby wywołać f na dowolnej ścieżce a-b, wystarczy napisać:
-  // Splay* old = a->root(); a->makeRoot(); b->applyF(f); old->makeRoot();
-  // Jeśli root jest bez znaczenia, można pominąć zmienną old.
-  void applyF(std::function<void(Splay*)> f) {
-    expose();
-    if (r == nullptr) {
-      f(this);
-    } else {
-      Splay* s = r;
-      while (s->l) {
-        s = s->l;
-        s->touch();
-      }
-      s->splay();
-      f(s->l);
-    }
-  }
-
-  // Sprawia, że wierzchołek jest rootem w swoim drzewie.
-  void makeRoot() {
-    applyF([](Splay* s) {
-      s->flip = !s->flip;
-    });
   }
 
   // Zakłada, że (*this) nie jest korzeniem drzewa.
   // Usuwa krawędź znajdującą się nad danym wierzchołkiem.
   void cut() {
-    expose();
-    assert(l != nullptr);  // jest rootem, nie można obciąć...
+    expose(); assert(l /* Nie jest rootem. */);
     Splay* s = l;
-    while (s->r) s = s->r;
-    s->splay();
-    s->r->p = nullptr;
-    s->r = nullptr;
+    while (s->touch(), s->r) s = s->r;
+    s->splay(); s->r->p = nullptr; s->r = nullptr;
   }
 
-  // Zwraca lca(this, v).
-  Splay* lca(Splay* v) {
-    expose();
-    v->expose();
-    return lastDisc;
+  void link(Splay* to) {
+    expose(); assert(!l /* Jest rootem. */);
+    p = to;
+    p->expose();        // SUBTREE
+    p->roz += axroz;    // SUBTREE
+    p->axroz += axroz;  // SUBTREE
   }
+
+  // Sprawia, że wierzchołek jest rootem w swoim drzewie oraz jest "exposed()".
+  void make_root() { expose(); flip = !flip; touch(); }
 };
-
-Splay* Splay::lastDisc;
