c8ff6f54d3d37041b6fbd51b800588dfd476971a
Konrad Paluszek <kezsulap@gmail.com>
2018-11-24 23:35:22 +0100

    Dodano ciąg De Bruijna (kradziony od Świstaków)

---
+++ code/konrad/debruijn.cpp
@@ -0,0 +1,18 @@
+//Generuje cykliczne słowo De Bruijn'a zawierające wszystkie podsłowa długości len nad alfabetem [0, alph)
+//Działa dla alph >= 2
+vector <int> de_bruijn(int len, int alph){
+	vector <int> res, lyn{0};
+	while (lyn[0] != alph - 1){
+		int l = lyn.size();
+		if (len % l == 0)
+			for (int c : lyn)
+				res.push_back(c);
+		for (int i = l; i <= len - 1; ++i)
+			lyn.push_back(lyn[i - l]);
+		while (lyn.back() == alph - 1)
+			lyn.pop_back();
+		lyn.back()++;
+	}
+	res.push_back(alph - 1);
+	return res;
+}
