ea23df654bea03d603f3be797acbdd54e044aeee
Konrad Paluszek <kezsulap@gmail.com>
2018-11-27 16:55:04 +0100

    Poprawka oznaczeń w DeBruijnie

--- code/konrad/debruijn.cpp
+++ code/konrad/debruijn.cpp
@@ -3,12 +3,12 @@
 vector <int> de_bruijn(int len, int alph){
 	vector <int> res, lyn{0};
 	while (lyn[0] != alph - 1){
-		int l = lyn.size();
-		if (len % l == 0)
+		int r = lyn.size();
+		if (len % r == 0)
 			for (int c : lyn)
 				res.push_back(c);
-		for (int i = l; i <= len - 1; ++i)
-			lyn.push_back(lyn[i - l]);
+		for (int i = r; i <= len - 1; ++i)
+			lyn.push_back(lyn[i - r]);
 		while (lyn.back() == alph - 1)
 			lyn.pop_back();
 		lyn.back()++;
