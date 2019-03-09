165414a5f65f4ac2de1fd845e5d7470614d78d4f
Konrad Paluszek <kezsulap@gmail.com>
2019-03-09 22:05:26 +0100

    Ufajnienie make.sh

--- code/finaly/make.sh
+++ code/finaly/make.sh
@@ -1,3 +1,3 @@
-find code -name '*.cpp' | xargs cat | a2ps -o lib.ps
+(for f in code/*.cpp; do echo -e "\n/*======  ${f#code/} ======*/"; cat $f; done) | sed 's/\t/  /g' | a2ps --center-title='' --header='Warsaw Eagles Team Reference Document' -o lib.ps
 ps2pdf lib.ps
 rm lib.ps*
