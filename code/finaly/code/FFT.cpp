97ea86ad25677b066546709842771d981e1d7c12
Konrad Paluszek <kezsulap@gmail.com>
2019-03-24 19:29:24 +0100

    FFT po Kamilu

--- code/finaly/code/FFT.cpp
+++ code/finaly/code/FFT.cpp
@@ -1,22 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
+/* Prec. error max_ans/1e15 (2.5e18) for (long) doubles, so int rounding works
+for doubles with answers 0.5e15, e.g. for sizes 2^20 and RANDOM ints in [0,45k],
+assuming DBL_MANT_DIG=53 and LDBL_MANT_DIG=64. Consider normalizing and brute.*/
 #define REP(i,n) for(int i = 0; i < int(n); ++i)
-/*Precision error max_ans/1e15 (2.5e18) for (long) doubles.
-So integer rounding works for doubles with answers 0.5*1e15,
-e.g. for sizes 2^20 and RANDOM positive integers up to 45k.
-Those values assume DBL_MANT_DIG=53 and LDBL_MANT_DIG=64.
-For input in [0, M], you can decrease everything by M/2.
-If there are many small vectors, uncomment "BRUTE FORCE".*/
 typedef double ld; // 'long double' is 2.2 times slower
-struct C {
-	ld re, im;
+struct C { ld re, im;
 	C operator * (const C & he) const {
 		return C{re * he.re - im * he.im,
 				re * he.im + im * he.re};
 	}
-	void operator += (const C & he) {
-		re += he.re; im += he.im;
-	}
+	void operator += (const C & he) { re += he.re; im += he.im; }
 };
 void dft(vector<C> & a, bool rev) {
 	const int n = a.size();
@@ -26,7 +18,7 @@ void dft(vector<C> & a, bool rev) {
 	}
 	for(int len = 1, who = 0; len < n; len *= 2, ++who) {
 		static vector<C> t[30];
-		auto & om = t[who];
+		vector<C> & om = t[who];
 		if(om.empty()) {
 			om.resize(len);
 			const ld ang = 2 * acosl(0) / len;
@@ -36,39 +28,50 @@ void dft(vector<C> & a, bool rev) {
 		for(int i = 0; i < n; i += 2 * len)
 			REP(k, len) {
 				 const C x = a[i+k], y = a[i+k+len]
-						* C{om[k].re, om[k].im * (1 - 2 * rev)};
+						* C{om[k].re, om[k].im * (rev ? -1 : 1)};
 				a[i+k] += y;
 				a[i+k+len] = C{x.re - y.re, x.im - y.im};
 			}
 	}
 	if(rev) REP(i, n) a[i].re /= n;
 }
-template<typename T>vector<T> multiply(const vector<T> & a, const vector<T> & b, bool split = false) {
+template<typename T>vector<T> multiply(const vector<T> & a, const vector<T> & b,
+		bool split = false, bool normalize = false) {
 	if(a.empty() || b.empty()) return {};
+	T big = 0; if(normalize) { // [0,B] into [-B/2, B/2]
+		assert(a.size() == b.size()); // equal size!!!
+		for(T x : a) big = max(big, x);
+		for(T x : b) big = max(big, x);
+		big /= 2;
+	}
 	int n = a.size() + b.size();
 	vector<T> ans(n - 1);
+	/* if(min(a.size(),b.size()) < 190) { // BRUTE FORCE
+		REP(i, a.size()) REP(j, b.size()) ans[i+j] += a[i]*b[j];
+		return ans; } */
 	while(n&(n-1)) ++n;
-	auto foo = [n](const vector<C> & w, int i, int k) {
-		int x = i ? n - i : 0, r = 1 - 2 * k;
-		return C{w[i].re + w[x].re * r, w[i].im
-				- w[x].im * r} * (k ? C{0, -0.5} : C{0.5, 0});
+	auto foo = [&](const vector<C> & w, int i, int k) {
+		int j = i ? n - i : 0, r = k ? -1 : 1;
+		return C{w[i].re + w[j].re * r, w[i].im
+				- w[j].im * r} * (k ? C{0, -0.5} : C{0.5, 0});
 	};
 	if(!split) { // standard fast version
 		vector<C> in(n), done(n);
-		REP(i, a.size()) in[i].re = a[i];
-		REP(i, b.size()) in[i].im = b[i];
+		REP(i, a.size()) in[i].re = a[i] - big;
+		REP(i, b.size()) in[i].im = b[i] - big;
 		dft(in, false);
 		REP(i, n) done[i] = foo(in, i, 0) * foo(in, i, 1);
 		dft(done, true);
 		REP(i, ans.size()) ans[i] = is_integral<T>::value ?
 				llround(done[i].re) : done[i].re;
+	//REP(i,ans.size())err=max(err,abs(done[i].re-ans[i]));
 	}
 	else { // Split big INTEGERS into pairs a1*M+a2,
-		const T M = 150; // where M = sqrt(max_absvalue).
+		const T M = 1<<15; // where M = sqrt(max_absvalue).
 		vector<C> t[2]; // This version is 2.2-2.5 times slower.
 		REP(x, 2) {
 			t[x].resize(n);
-			auto & in = x ? b : a;
+			auto & in = x ? b : a; // below use (in[i]-big) if normalized
 			REP(i, in.size()) t[x][i]=C{ld(in[i]%M), ld(in[i]/M)};
 			dft(t[x], false);
 		}
@@ -77,8 +80,16 @@ template<typename T>vector<T> multiply(const vector<T> & a, const vector<T> & b,
 			vector<C> prod(n);
 			REP(x, 2) REP(y, 2) if(x + y == s) REP(i, n)
 				prod[i] += foo(t[0], i, x) * foo(t[1], i, y);
-			dft(prod, true);
+			dft(prod, true); // remember: llround(prod[i].re)%MOD*mul !!!
 			REP(i, ans.size()) ans[i]+= llround(prod[i].re)*mul;
+		}
+	}
+	if(normalize) {
+		T so_far = 0;
+		REP(i, ans.size()) {
+			if(i < (int) a.size()) so_far += a[i] + b[i];
+			else so_far -= a[i-a.size()] + b[i-a.size()];
+			ans[i] += big * so_far - big * big * min(i + 1, (int) ans.size() - i);
 		}
 	}
 	return ans;
